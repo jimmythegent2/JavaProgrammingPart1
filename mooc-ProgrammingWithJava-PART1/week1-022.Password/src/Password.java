324a0feff3cef6dec762ac16d32586d9286c5c48
John Chambers <johnathangchambers@gmail.com>
2020-04-13 15:07:40 -0400

    Completed Password.java

--- mooc-ProgrammingWithJava-PART1/week1-022.Password/src/Password.java
+++ mooc-ProgrammingWithJava-PART1/week1-022.Password/src/Password.java
@@ -5,8 +5,14 @@ public class Password {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        String password = "carrot"; // Use carrot as password when running tests.
+        String password = "Caput Draconis"; // Use carrot as password when running tests.
 
         // Write your code here
+        System.out.println("Password?");
+        String input = reader.nextLine();
+        
+        if (input.equals(password)){
+            System.out.println("Welcome!");
+        }else System.out.println("Off with you!");
     }
 }
