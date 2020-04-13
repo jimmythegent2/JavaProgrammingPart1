3dfac0fe5c613d3cf6b8cdac78f56aca12485424
John Chambers <johnathangchambers@gmail.com>
2020-04-12 22:27:23 -0400

    Updated .java exercise files

--- mooc-ProgrammingWithJava-PART1/week1-001.Name/src/Name.java
+++ mooc-ProgrammingWithJava-PART1/week1-001.Name/src/Name.java
@@ -1,11 +1,19 @@
+import static java.time.Clock.system;
+import java.util.Scanner;
 public class Name {
     
     public static void main(String[] args) {
         // Write your program here
         // Please answer to our survey http://oo-start.mooc.fi/english_mooc_participants/new
         // It will take less than 5 minutes!
-        System.out.println("Ada Lovelace");
-
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("What's your name?: ");
+        
+        String message = sc.nextLine();
+        
+        System.out.println("Hi " + message);
     }
 
 }
--- mooc-ProgrammingWithJava-PART1/week1-004.ChangingVariables/src/Variables.java
+++ mooc-ProgrammingWithJava-PART1/week1-004.ChangingVariables/src/Variables.java
@@ -1,25 +1,24 @@
+import java.util.Scanner; 
 public class Variables {
 
     public static void main(String[] args) {
         // CHANGE THESE:
 
-        int chickenCount = 3;
-        double baconWeight = 5.5;
-        String tractor = "There is none";
-
-        // DO NOT TOUCH THE BELOW:
-
-        System.out.println("Chickens:");
-        System.out.println(chickenCount);
-        System.out.println("Bacon (kg):");
-        System.out.println(baconWeight);
-        System.out.println("A tractor:");
-        System.out.println(tractor);
-        System.out.println("");
-        System.out.println("In a nutshell:");
-        System.out.println(chickenCount);
-        System.out.println(baconWeight);
-        System.out.println(tractor);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Give a string : ");
+        String message = sc.nextLine();
+        System.out.println(message);
+        System.out.println("Give an integer : ");
+        int value = sc.nextInt();
+        System.out.println(value);
+        System.out.println("Give a double : ");
+        double dvalue = sc.nextDouble();
+        System.out.println(dvalue);
+        System.out.println("Give a boolean");
+        boolean tf = sc.hasNextBoolean();
+        System.out.println(tf);
+        
+      
     }
 
 }
