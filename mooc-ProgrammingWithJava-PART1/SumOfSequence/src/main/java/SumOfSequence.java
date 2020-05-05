d8242395225e464337b69019f52c939b29ba189a
myusername <johanthangchambers@gmail.com>
2020-05-05 12:59:42 -0400

    Sequencepart2

--- mooc-ProgrammingWithJava-PART1/SumOfSequence/src/main/java/SumOfSequence.java
+++ mooc-ProgrammingWithJava-PART1/SumOfSequence/src/main/java/SumOfSequence.java
@@ -12,10 +12,12 @@ import java.util.Scanner;
 public class SumOfSequence {
     public static void main (String args []){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Last number?");
+        System.out.println("First Number?");
         int input = Integer.valueOf(scanner.nextLine());
+        System.out.println("Last number?");
+        int input2 = Integer.valueOf(scanner.nextLine());
         int result = 0;
-        for (int i = 1; i <= input; i++){
+        for (int i = input; i <= input2; i++){
             
             result += i;
         }
